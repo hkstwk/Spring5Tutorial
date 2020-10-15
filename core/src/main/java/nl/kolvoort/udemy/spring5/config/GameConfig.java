31c7db56e399831e7290a882292ec9876cee30d9
hkstwk <hkolvoort+github@gmail.com>
2020-10-15 15:48:08 +0200

    Udemy Spring5 Course, completed Section 5, lesson 41 Using Properties

--- core/src/main/java/nl/kolvoort/udemy/spring5/config/GameConfig.java
+++ core/src/main/java/nl/kolvoort/udemy/spring5/config/GameConfig.java
@@ -2,16 +2,21 @@ package nl.kolvoort.udemy.spring5.config;
 
 import nl.kolvoort.udemy.spring5.GuessCount;
 import nl.kolvoort.udemy.spring5.MaxNumber;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
+@PropertySource("classpath:config/game.properties")
 public class GameConfig {
 
     // fields
-    private int maxNumber = 25;
+    @Value("${game.maxNumber:20}")
+    private int maxNumber;
 
-    private int guessCount = 8;
+    @Value("${game.guessCount:5}")
+    private int guessCount;
 
     // bean methods
     @Bean
