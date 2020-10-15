df396db9be9e2684408942059cc2761ba4952c92
hkstwk <hkolvoort+github@gmail.com>
2020-10-15 21:56:24 +0200

    Udemy Spring5 Course, completed Section 6, lesson 47 Using Lombok

--- core/src/main/java/nl/kolvoort/udemy/spring5/GameImpl.java
+++ core/src/main/java/nl/kolvoort/udemy/spring5/GameImpl.java
@@ -1,29 +1,33 @@
 package nl.kolvoort.udemy.spring5;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 
+@Slf4j
+@Getter
 @Component
 public class GameImpl implements Game {
 
-    // Constants
-    private static final Logger log = LoggerFactory.getLogger(GameImpl.class);
-
     // Fields
+    @Getter(AccessLevel.NONE)
     private final NumberGenerator numberGenerator;
-    private final int guessCount;
 
+    private final int guessCount;
     private int number;
-    private int guess;
     private int smallest;
     private int biggest;
     private int remainingGuesses;
     private boolean validNumberRange = true;
+
+    @Setter
+    private int guess;
 
     // constructor
     @Autowired
@@ -50,42 +54,7 @@ public class GameImpl implements Game {
     }
 
     // public methods
-    @Override
-    public int getNumber() {
-        return number;
-    }
-
-    @Override
-    public int getGuess() {
-        return guess;
-    }
-
-    @Override
-    public void setGuess(int guess) {
-        this.guess = guess;
-    }
-
-    @Override
-    public int getSmallest() {
-        return smallest;
-    }
-
-    @Override
-    public int getBiggest() {
-        return biggest;
-    }
-
-    @Override
-    public int getRemainingGuesses() {
-        return remainingGuesses;
-    }
-
-    @Override
-    public int getGuessCount() {
-        return guessCount;
-    }
-
-    @Override
+   @Override
     public void check() {
         checkValidNumberRange();
 
@@ -100,11 +69,6 @@ public class GameImpl implements Game {
 
             remainingGuesses--;
         }
-    }
-
-    @Override
-    public boolean isValidNumberRange() {
-        return validNumberRange;
     }
 
     @Override
