5ccf0d3516c4bf495faca8b5301b9726d250d008
hkstwk <hkolvoort+github@gmail.com>
2020-10-15 22:05:45 +0200

    Udemy Spring5 Course, completed Section 6, lesson 48 Lombok Challenge

--- console/src/main/java/nl/kolvoort/udemy/spring5/console/ConsoleNumberGuess.java
+++ console/src/main/java/nl/kolvoort/udemy/spring5/console/ConsoleNumberGuess.java
@@ -1,21 +1,18 @@
 package nl.kolvoort.udemy.spring5.console;
 
 
+import lombok.extern.slf4j.Slf4j;
 import nl.kolvoort.udemy.spring5.Game;
 import nl.kolvoort.udemy.spring5.MessageGenerator;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
 import java.util.Scanner;
 
+@Slf4j
 @Component
 public class ConsoleNumberGuess {
-
-    // constants
-    private static final Logger log = LoggerFactory.getLogger(ConsoleNumberGuess.class);
 
     // fields
     private final Game game;
