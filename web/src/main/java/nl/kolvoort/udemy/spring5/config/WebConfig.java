96cfcca0aab830bbba7f53af517f7f1fdaa6e4c1
hkstwk <hkolvoort+github@gmail.com>
2020-10-24 21:31:19 +0200

    Udemy Spring5 Course, lesson 80 - Setup Thymeleaf and Initial Template

---
+++ web/src/main/java/nl/kolvoort/udemy/spring5/config/WebConfig.java
@@ -0,0 +1,15 @@
+package nl.kolvoort.udemy.spring5.config;
+
+import nl.kolvoort.udemy.spring5.util.ViewNames;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName(ViewNames.HOME);
+    }
+}
---
+++ web/src/main/java/nl/kolvoort/udemy/spring5/util/ViewNames.java
@@ -0,0 +1,13 @@
+package nl.kolvoort.udemy.spring5.util;
+
+public final class ViewNames {
+
+    // constants
+    public static final String HOME = "home";
+
+    // constructors
+    private ViewNames() {}
+
+
+
+}
